6ec1acb2aebb338e674541693863d1f6a6e6e1a2
Michael Rose <michael.rose@figure.ai>
2022-10-26 17:20:50 -0700

    Remove duplicate code

--- visualizer_3d/visualizer_3d_widget.py
+++ visualizer_3d/visualizer_3d_widget.py
@@ -15,6 +15,7 @@ from urdfpy import URDF
 from checkable_combo_box import CheckableComboBox
 from friction_cone import FrictionCone
 from robot_model import RobotModel, RobotObjProxy
+import utils
 
 _DIR_ = ("x", "y", "z")
 _QUAT_ = ("qx", "qy", "qz", "qw")
@@ -158,50 +159,7 @@ class Visualizer3DWidget(GLViewWidget):
         return robot
 
     def addAxis(self, *args, **kwargs):
-        size = kwargs.get('size', 0.1)
-        position = kwargs.get("translation", [0, 0, 0])
-
-        if len(position) != 3:
-            print(f"Invalid position. Must contain 3 elements: '{position}'")
-            return None
-
-        R = Rotation.identity()
-        try:
-            rot_type = None
-            rot_val = None
-            if 'quaternion' in kwargs:
-                rot_type = "quaternion"
-                rot_val = kwargs[rot_type]
-                try:
-                    R = Rotation.from_quat(rot_val)
-                except:
-                    R = Rotation.from_quat([rot_val[i] for i in _QUAT_])
-            elif 'axis_angle' in kwargs:
-                rot_type = "axis_angle"
-                rot_val = kwargs[rot_type]
-                R = Rotation.from_rotvec(rot_val)
-            elif 'rotation_matrix' in kwargs:
-                rot_type = "rotation_matrix"
-                rot_val = kwargs[rot_type]
-                R = Rotation.from_matrix(rot_val)
-        except:
-            print(f"Invalid rotation of type '{rot_type}' with value: '{rot_val}'")
-            return None
-
-        new_triad = gl.GLAxisItem(glOptions='opaque')
-        new_triad.setSize(x=size, y=size, z=size)
-
-        new_triad.resetTransform()
-
-        # GLAxisItem expects an axis/angle representation
-        ax_ang = R.as_rotvec()
-        ang = np.linalg.norm(ax_ang)
-        axis = np.array([0, 0, 1])
-        if ang != 0:
-            axis = ax_ang / ang
-
-        new_triad.rotate(ang * 180 / np.pi, *axis)
-        new_triad.translate(*position)
+        new_triad = utils.createAxis(args, kwargs)
 
         self._axes.append(new_triad)
         self.addItem(new_triad)
